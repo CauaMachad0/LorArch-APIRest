faa24d44e094ee02b7e14a7e1b96035a41a8b77a
CauaMachad0 <caua.marcelo.machado@outlook.com>
2025-05-13 20:29:06 -0300

    Ajuste do código novamente

--- src/main/java/com/lorarch/challenge/LocalizadorMotos.java
+++ src/main/java/com/lorarch/challenge/LocalizadorMotos.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LocalizadorMotos {
 
     public static void main(String[] args) {
-        SpringApplication.run(LocalizadorMotosApplication.class, args);
+        SpringApplication.run(LocalizadorMotos.class, args);
     }
 }
