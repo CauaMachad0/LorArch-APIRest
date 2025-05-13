ff6dbade1bc5c6e5318b40359850a8c87234cd48
CauaMachad0 <caua.marcelo.machado@outlook.com>
2025-05-13 20:26:27 -0300

    Criando o projeto

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+}
+
+group = "com.lorarch"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/lorarch/challenge/LocalizadorMotos.java
@@ -0,0 +1,12 @@
+package com.lorarch.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizadorMotosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalizadorMotosApplication.class, args);
+    }
+}
