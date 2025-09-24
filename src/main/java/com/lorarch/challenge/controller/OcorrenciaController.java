cdd7490d0be297472d4b76c5f237cfe0463d37d0
CauaMachad0 <caua.marcelo.machado@outlook.com>
2025-09-23 22:20:05 -0300

    Subindo os Controllers

--- src/main/java/com/lorarch/challenge/controller/OcorrenciaController.java
+++ src/main/java/com/lorarch/challenge/controller/OcorrenciaController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ocorrencias")
+@RequestMapping("/api/ocorrencias")
 public class OcorrenciaController {
 
     @Autowired
