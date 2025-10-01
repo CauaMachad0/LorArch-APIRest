fba4f0a5bbd513389f42fb28fe3d2c93133d4852
CauaMachad0 <caua.marcelo.machado@outlook.com>
2025-10-01 19:00:39 -0300

    Arrumando o projeto todo estando dando muitos problemas!

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.flywaydb.flyway") version "9.22.3"
 }
 
 group = "com.lorarch"
@@ -16,6 +17,7 @@ java {
 repositories {
     mavenCentral()
 }
+
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
